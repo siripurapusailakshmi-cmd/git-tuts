edd32fa8aa6ff5ac8b533154642e9874b7bce464
siripurapusailakshmi-cmd <siripurapu_sailakshmi@epam.com>
2026-03-16 01:03:30 +0530

    Delete tests/clientWork.spec.ts

--- tests/clientWork.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-// Test: EPAM Client Work navigation
-test.describe('EPAM site - Client Work navigation', () => {
-  test('should navigate to Client Work page and display "Client Work"', async ({ page }) => {
-    // TEST_BODY_PLACEHOLDER
-  });
-});
